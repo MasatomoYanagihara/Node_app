acf72727c79017983c830a73e0f0dee3c8d257ac
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-20 22:42:17 +0900

    response.setHeader()使用
    ContentTypeをtext/htmlにする。
    response.wirte()でHTML出力

--- app.js
+++ app.js
@@ -12,8 +12,19 @@ serverオブジェクトを作成
 createServerではrequestとresponseの二つの引数を必ず用意する。
 */
 var server = http.createServer((request, response) => {
-  response.end("Hello World!");
+  /* ResponseHeadersのContent-Typeをtext/htmlにする */
+  response.setHeader("Content-Type", "text/html");
+
+  response.write('<!DOCTYPE html></html lang="ja">');
+  response.write('<head><meta charset="utf-8">');
+  response.write("<titile>Hello</title></head>");
+  response.write("<body><h1>Hello Node.js</h1>");
+  response.write("<p>This is Node.js sample page.</p>");
+  response.write("<p>これは、Node.jsのサンプルページです。</p>", "utf-8");
+  response.write("</body></html>");
+  response.end();
 });
 
 /* サーバーを待ち受け状態にする */
 server.listen(3000);
+console.log("Server start!");
