82805c13ff0c59249b34419ef911c8bb4acf02e6
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-20 23:04:21 +0900

    fs.readFile()使用
    別のHTMLファイルを読み込み表示

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
-/*
-Hello Worldを出力するプログラム
-localhost:3000で起動している。
-3000番ポートはNode.jsでデフォルトで使われている。
-*/
-
-/* httpモジュールをロード */
 const http = require("http");
+const fs = require("fs"); //fsモジュールをロード
 
-/*
-serverオブジェクトを作成
-createServerではrequestとresponseの二つの引数を必ず用意する。
-*/
 var server = http.createServer((request, response) => {
-  /* ResponseHeadersのContent-Typeをtext/htmlにする */
-  response.setHeader("Content-Type", "text/html");
-
-  response.write('<!DOCTYPE html></html lang="ja">');
-  response.write('<head><meta charset="utf-8">');
-  response.write("<titile>Hello</title></head>");
-  response.write("<body><h1>Hello Node.js</h1>");
-  response.write("<p>This is Node.js sample page.</p>");
-  response.write("<p>これは、Node.jsのサンプルページです。</p>", "utf-8");
-  response.write("</body></html>");
-  response.end();
+  /*
+  ファイルを読み込んでいる。
+  fs.readFile(ファイル名, エンコーディング, readが完了した後に実行する関数(コールバック関数))
+   */
+  fs.readFile("./index.html", "UTF-8", (error, data) => {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(data);
+    response.end();
+  });
 });
 
-/* サーバーを待ち受け状態にする */
 server.listen(3000);
 console.log("Server start!");
