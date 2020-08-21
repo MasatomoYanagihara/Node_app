4dd52dc042f29ae2379192ff4f1d118a06f34fb4
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-21 21:15:51 +0900

    簡易伝言ページ

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
 const url = require("url");
-const qs = require("querystring"); //queryモジュールをロード
+const qs = require("querystring");
 
 const index_page = fs.readFileSync("./index.ejs", "utf8");
 const other_page = fs.readFileSync("./other.ejs", "utf8");
@@ -31,63 +31,63 @@ function getFromClient(request, response) {
   }
 }
 
-/* 繰り返し表示用のデータを用意 */
-
 var data = {
-  Taro: "09-999-999",
-  Hanako: "080-888-888",
-  Sachiko: "070-777-777",
-  Ichiro: "060-666-666",
+  msg: "no message...",
 };
 
 // indexのアクセス処理
 function response_index(request, response) {
-  var msg = "これはIndexページです。";
+  /* POSTアクセス時の処理 */
+  if (request.method == "POST") {
+    var body = "";
+
+    /* データ受信のイベント処理 */
+    request.on("data", (data) => {
+      body += data;
+    });
+
+    /* データ受信終了のイベント処理 */
+    request.on("end", () => {
+      data = qs.parse(body);
+      write_index(request, response);
+    });
+  } else {
+    write_index(request, response);
+  }
+}
+
+/* indexの表示の作成 */
+function write_index(request, response) {
+  var msg = "伝言を表示します。";
   var content = ejs.render(index_page, {
     title: "Index",
     content: msg,
     data: data,
-    filename: 'data_item' //パーシャルファイルを指定
   });
   response.writeHead(200, { "Content-Type": "text/html" });
   response.write(content);
   response.end();
 }
 
+var data2 = {
+  Taro: ["taro@yamada", "09-999-999", "Tokyo"],
+  Hanako: ["hanako@flower", "080-888-888", "Yokohama"],
+  Sachiko: ["sachi@happy", "070-777-777", "Nagoya"],
+  Ichiro: ["ichi@baseball", "060-666-666", "USA"],
+};
+
 /* Otherのアクセス処理 */
 function response_other(request, response) {
   var msg = "これはOtherページです。";
 
-  /* POSTアクセス時の処理 */
-  if (request.method == "POST") {
-    var body = "";
+  var content = ejs.render(other_page, {
+    title: "Other",
+    content: msg,
+    data: data2,
+    filename: "data_item",
+  });
 
-    /* データ受信のイベント(data) */
-    request.on("data", (getdata) => {
-      body += getdata;
-    });
-
-    /* データ受信終了のイベント(end)処理 */
-    request.on("end", () => {
-      var post_data = qs.parse(body);
-      msg += "あなたは、「" + post_data.msg + "」と書きました。";
-      var content = ejs.render(other_page, {
-        title: "Other",
-        content: msg,
-      });
-      response.writeHead(200, { "Content-Type": "text/html" });
-      response.write(content);
-      response.end();
-    });
-  } else {
-    /* GETアクセス時の処理 */
-    var msg = "ページがありません。";
-    var content = ejs.render(other_page, {
-      title: "Other",
-      content: msg,
-    });
-    response.writeHead(200, { "Content-Type": "text/html" });
-    response.write(content);
-    response.end();
-  }
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.write(content);
+  response.end();
 }
