1004309211e8a246d8c96bb0f2b6fe9bc197d3a6
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-21 16:00:44 +0900

    queryの取り出し方
    URLに付加するクエリパラメータの取り出し方

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const http = require("http");
 const fs = require("fs");
 const ejs = require("ejs");
-const url = require("url"); //urlモジュールをロード
+const url = require("url");
 
 const index_page = fs.readFileSync("./index.ejs", "utf8");
-const style_css = fs.readFileSync("./style.css", "utf8"); //style.cssを読み込み
+const other_page = fs.readFileSync("./other.ejs", "utf8");
 
 var server = http.createServer(getFromClient);
 
@@ -12,26 +12,24 @@ server.listen(3000);
 console.log("Server start!");
 
 function getFromClient(request, response) {
+  var url_parts = url.parse(request.url, true); //第２引数にtrueでqueryをオブジェクトにする
 
-  /* url.parse()でURLの構成要素に分ける */
-  var url_parts = url.parse(request.url);
-
-  /* ルーティング処理 */
   switch (url_parts.pathname) {
-
     case "/":
+      var content = "これはIndexページです。";
+
+      var query = url_parts.query; //queryを取り出している
+
+      if (query.msg != undefined) { //queryがない場合はundifinedになる
+        content += "あなたは、「" + query.msg + "」と送りました。";
+      }
+
       var content = ejs.render(index_page, {
         title: "Index",
-        content: "これはテンプレートを使ったサンプルページです。",
+        content: content,
       });
       response.writeHead(200, { "Content-Type": "text/html" });
       response.write(content);
-      response.end();
-      break;
-
-    case "/style.css":
-      response.writeHead(200, { "Content-Type": "text/css" });
-      response.write(style_css);
       response.end();
       break;
 
