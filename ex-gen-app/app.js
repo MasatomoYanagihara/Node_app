8f1ea05e4d699b5509566c4281fec00dd6c8c2a3
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-22 16:57:39 +0900

    queryの取り出し方（Express）

--- ex-gen-app/app.js
+++ ex-gen-app/app.js
@@ -1,49 +1,54 @@
 /* 必要なモジュールのロード */
-var createError = require('http-errors'); //HTTPエラーの対処を行う為のもの
-var express = require('express'); //Express本体
-var path = require('path'); //ファイルパスを扱う為のもの
-var cookieParser = require('cookie-parser'); //クッキーのパースに関するもの
-var logger = require('morgan'); //HTTPリクエストのログ出力に関するもの
+var createError = require("http-errors"); //HTTPエラーの対処を行う為のもの
+var express = require("express"); //Express本体
+var path = require("path"); //ファイルパスを扱う為のもの
+var cookieParser = require("cookie-parser"); //クッキーのパースに関するもの
+var logger = require("morgan"); //HTTPリクエストのログ出力に関するもの
 
-/* ルート用モジュールのロード */
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var helloRouter = require('./routes/hello');
+/* Expressオブジェクトの作成 */
 
-/* Expressオブジェクトの作成 */ 
 var app = express();
 
-/* Expressオブジェクトの設定 */ 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+/* Expressオブジェクトの設定 */
 
-/* 関数組み込み */ 
-app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+/* 関数組み込み */
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-/* アクセスするルート設定 */ 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/hello', helloRouter);
+/* ルート用モジュールのロード */
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var helloRouter = require("./routes/hello");
 
-/* エラー用のapp.use作成 */ 
-app.use(function(req, res, next) {
+/* アクセスするルート設定 */
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/hello", helloRouter);
+
+/* エラー用のapp.use作成 */
+
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
-/* module.expressの設定 */ 
+/* module.expressの設定 */
+
 module.exports = app;
--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -2,10 +2,18 @@ var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
+/* 第１引数は"/hello"ではなく"/"となる。"/"は/hello/という意味。ルーティングはapp.jsでしている。 */
 router.get("/", function (req, res, next) {
+
+  /* queryを取り出している。 */ 
+  var name = req.query.name;
+  var mail = req.query.mail;
+
   var data = {
     title: "Hello!",
-    content: "これは、サンプルのコンテンツです。<br>This is sample content.",
+    content:
+      "あなたの名前は、" + name + "。<br>" +
+      "メールアドレスは、" + mail + "です。",
   };
 
   res.render("hello", data);
