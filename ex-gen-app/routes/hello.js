3af45538bad8ab74ff4c46e41c139c218dee258d
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-23 13:38:03 +0900

    idを指定して一件だけ表示する
    db.get()とwhere句を使用。

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const sqlite3 = require("sqlite3"); 
+const sqlite3 = require("sqlite3");
 
-// データベースオブジェクトの取得
 const db = new sqlite3.Database("mydb.sqlite3");
 
-// GETアクセスの処理
 router.get("/", (req, res, next) => {
-  // データベースのシリアライズ
+
   db.serialize(() => {
-    //レコードを全て取り出す
+
     db.all("select * from mydata", (err, rows) => {
       // データベースアクセス完了時の処理
       if (!err) {
@@ -18,7 +16,48 @@ router.get("/", (req, res, next) => {
           title: "Hello!",
           content: rows, // 取得したレコードデータ
         };
-        res.render("hello", data);
+        res.render("hello/index", data);
+      }
+    });
+  });
+});
+
+/* hello/add/でのGET処理 */
+router.get("/add", (req, res, next) => {
+  var data = {
+    title: "Hello/Add",
+    content: "新しいレコードを入力:",
+  };
+  res.render("hello/add", data);
+});
+
+/* hello/add/でのPOST処理 */
+router.post("/add", (req, res, next) => {
+  const nm = req.body.name;
+  const ml = req.body.mail;
+  const ag = req.body.age;
+
+  db.serialize(() => {
+    db.run("insert into mydata (name, mail, age) values (?, ?, ?)", nm, ml, ag);
+  });
+  res.redirect("/hello");
+});
+
+/* hello/show/でのGET処理 */
+router.get("/show", (req, res, next) => {
+  const id = req.query.id; //idを取り出している
+
+  db.serialize(() => {
+    // db.get()は一件だけ取り出す
+    db.get("select * from mydata where id = ?", [id], (err, row) => {
+      if (!err) {
+        var data = {
+          title: "Hello/show",
+          content: "id = " + id + " のレコード：",
+          mydata: row,
+        };
+
+        res.render("hello/show", data);
       }
     });
   });
