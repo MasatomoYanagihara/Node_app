3a425e9d7b6b19fb25275b2195468d940639bc0e
Yana <masatomo.yanagihara.jp@gmail.com>
2020-08-23 19:39:01 +0900

    Opから演算子を指定して検索
    sequelizeのOperatorオブジェクトを使用。

--- ex-gen-app/routes/users.js
+++ ex-gen-app/routes/users.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../models/index"); //Sequelizeで利用される全ての情報が入っている。
+const { Op } = require("sequelize"); //Operatorオブジェクトをロード
 
 /* GET users listing. */
 router.get("/", (req, res, next) => {
   const id = req.query.id;
   db.User.findAll({
     where: {
-      id: id,
+      id: { [Op.lte]: id } //lte(less than/equal)は以下
+      // id: { [Op.gte]: id } gte(greater than/equal)は以上
+      // id: { [Op.lt]: id } lt(less than)は未満
+      // id: { [Op.lt]: id } lt(less than)は超える
     },
   }).then((usrs) => {
     var data = {
